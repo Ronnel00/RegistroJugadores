21a4ca93889b14ce105f74f86eb2517530a27efe
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-09-02 21:22:10 -0400

    Creando el modelo

---
+++ Models/Jugadores.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroJugadores.Models
+{
+    public class Jugadores
+    {
+        [Key]
+        public int jugadorId { get; set; }
+
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "El campo Partida es obligatorio.")]
+        [Range(0, int.MaxValue, ErrorMessage = "El numero de partidas debe ser un valor valido.")]
+        public int Partida { get; set; }
+    }
+}
