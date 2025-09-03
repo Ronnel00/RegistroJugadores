f2471a2f31fc7236127034446a0115369968b6d4
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-09-02 21:30:27 -0400

    Creando el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroJugadores.Models;
+
+namespace RegistroJugadores.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base (options) { }
+
+        public DbSet<Jugadores> Jugadores { get; set; }
+    }
+}
