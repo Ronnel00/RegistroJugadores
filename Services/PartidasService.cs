11320a9aa7d87116ceb53db0f0febf869464e106
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-09-11 10:48:14 -0400

    Creando PartidasService

---
+++ Services/PartidasService.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using RegistroJugadores.DAL;
+using RegistroJugadores.Models;
+
+namespace RegistroJugadores.Services
+
+    {
+        public class PartidasService(IDbContextFactory<Contexto> DbFactory)
+        {
+            public async Task<bool> Guardar(Partidas partida)
+            {
+                if (!await Existe(partida.PartidaId))
+                {
+                    return await Insertar(partida);
+                }
+                else
+                {
+                    return await Modificar(partida);
+                }
+            }
+
+            public async Task<bool> Existe(int PartidaId)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                return await contexto.Partidas.AnyAsync(p => p.PartidaId == PartidaId);
+            }
+
+            private async Task<bool> Insertar(Partidas partida)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                contexto.Partidas.Add(partida);
+                return await contexto.SaveChangesAsync() > 0;
+            }
+
+            private async Task<bool> Modificar(Partidas partida)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                contexto.Update(partida);
+                return await contexto.SaveChangesAsync() > 0;
+            }
+
+            public async Task<Partidas?> Buscar(int PartidaId)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                return await contexto.Partidas
+                .Include(p => p.Jugador1)
+                .Include(p => p.Jugador2)
+                .Include(p => p.Ganador)
+                .Include(p => p.TurnoJugador)
+                .FirstOrDefaultAsync(p => p.PartidaId == PartidaId);
+            }
+            public async Task<bool> Eliminar(int PartidaId)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                return await contexto.Partidas.AsNoTracking().Where(p => p.PartidaId == PartidaId).ExecuteDeleteAsync() > 0;
+            }
+            public async Task<List<Partidas>> Listar(Expression<Func<Partidas, bool>> criterio)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                return await contexto.Partidas
+                    .Include(p => p.Jugador1)
+                    .Include(p => p.Jugador2)
+                    .Include(p => p.Ganador)
+                    .Include(p => p.TurnoJugador)
+                    .Where(criterio)
+                    .AsNoTracking()
+                    .ToListAsync();
+            }
+        }
+    }
