870e466b85f0ab07a57bfe79343529f69e01a0e9
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-09-11 01:51:00 -0400

    modificar Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,5 +8,6 @@ namespace RegistroJugadores.DAL
         public Contexto(DbContextOptions<Contexto> options) : base (options) { }
 
         public DbSet<Jugadores> Jugadores { get; set; }
+        public DbSet<Partidas> Partidas { get; set; }
     }
 }
