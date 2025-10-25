6cbe4ab66b9971b0bc958a2fade0353b2c8ee179
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-10-25 17:00:27 -0400

    Creando los DTO

---
+++ RegistroJugadores.BlazorWasm/DTO/JugadorRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace RegistroJugadores.ApiDTO;
+
+public class JugadorRequest
+{
+    public string Nombres { get; set; }
+    public string Email { get; set; }
+}
---
+++ RegistroJugadores.BlazorWasm/DTO/JugadorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace RegistroJugadores.ApiDTO;
+
+public class JugadorResponse
+{
+    public int JugadorId { get; set; }
+    public string Nombres { get; set; }
+    public string Email { get; set; }
+}
