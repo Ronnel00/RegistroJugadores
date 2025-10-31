5fd68ca7b2aab3d0ca2acd2f732037f701bad3ee
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-10-31 00:54:23 -0400

    Creando los DTO

---
+++ DTO/MovimientosRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistroJugadores.ApiDTO;
+
+public class MovimientosRequest
+{
+    public int PartidaId { get; set; }
+
+    public string Jugador { get; set; } = "X";
+
+    public int PosicionFila { get; set; }
+
+    public int PosicionColumna { get; set; }
+}
